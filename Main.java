00ee9065f0e134251cd0e7b26873aae3e2191cea
Gaston González <gaston@novateksoluciones.com>
2021-07-16 18:02:58 -0300

    Todo OK

--- Main.java
+++ Main.java
@@ -2,6 +2,5 @@ class Main{
 	public static void main(String[] args){
 		System.out.println("Hola mundo, desde java");
 		System.out.println("La conexión con Github se ha realizado");
-		estoesunerror
 	}
 }
