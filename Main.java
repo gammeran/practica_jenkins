dd0037040b028f258c05e06a24b0bc4603bee1ab
Gaston González <gaston@novateksoluciones.com>
2021-07-16 18:00:46 -0300

    error a propósito

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 class Main{
 	public static void main(String[] args){
 		System.out.println("Hola mundo, desde java");
-		System.out.println("La conexión con Github se ha realizado")
+		System.out.println("La conexión con Github se ha realizado");
+		estoesunerror
 	}
 }
